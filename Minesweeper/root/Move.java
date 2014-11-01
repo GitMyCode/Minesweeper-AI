ff0eb0ace5156da56d5af993fb12453882fb7984
GitMyCode <marbouchl@gmail.com>
2014-10-31 20:36:51 -0400

    CSP marche partiellement il ne pose pas les flags

--- Minesweeper/root/Move.java
+++ Minesweeper/root/Move.java
@@ -13,4 +13,24 @@ public class Move {
         this.coup = coup;
         this.index = index;
     }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Move move = (Move) o;
+
+        if (index != move.index) return false;
+        if (coup != move.coup) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode () {
+        int result = coup != null ? coup.hashCode() : 0;
+        result = 31 * result + index;
+        return result;
+    }
 }
