bc3a65964a7d6fc2672a2af3c7d09d4354821e2b
GitMyCode <marbouchl@gmail.com>
2014-11-05 12:19:23 -0500

    Head: Merge Step Button Description: 	Il y a des residu d'un prochaine banche dasn CSP. 	J'ai commencer a faire des methodes pour separer les frontieres 	indépendante.

--- Minesweeper/root/ai/utilCSP/Util.java
+++ Minesweeper/root/ai/utilCSP/Util.java
@@ -4,6 +4,7 @@ import root.ENUM.CASE;
 import root.ENUM.COUP;
 import root.Move;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -53,6 +54,112 @@ public class Util {
                 grid[m.index] = CASE.DEFUSED; //Just pour une valeur quelquonc
             }
         }
+
+
+        for(CASE c : grid){
+            if(c == CASE.UNDISCOVERED){
+                print+= '□';
+            }else if(c == CASE.EMPTY) {
+                print+='_';
+            }else if(c == CASE.FLAGED){
+                print+= '⚑';
+
+            }else if(c == CASE.BLOW) {
+                print+= '●';
+            }else if(c== CASE.DEFUSED){
+                print+= '◯'; //○
+            }else {
+                print += c.indexValue;
+            }
+            if(i%nbcol ==0 && i!=0){
+                print += "\n";
+            }
+            i++;
+        }
+        System.out.println(print);
+
+    }
+
+    public static void printFrontiere(CASE[] gridOrigin, int nbcol,Set<Integer> frontiere){
+
+        String print="";
+
+        int i=1;
+        CASE[] grid = gridOrigin.clone();
+
+        for(Integer f : frontiere){
+            grid[f] = CASE.BLOW;
+        }
+
+
+        for(CASE c : grid){
+            if(c == CASE.UNDISCOVERED){
+                print+= '□';
+            }else if(c == CASE.EMPTY) {
+                print+='_';
+            }else if(c == CASE.FLAGED){
+                print+= '⚑';
+
+            }else if(c == CASE.BLOW) {
+                print+= '●';
+            }else if(c== CASE.DEFUSED){
+                print+= '◯'; //○
+            }else {
+                print += c.indexValue;
+            }
+            if(i%nbcol ==0 && i!=0){
+                print += "\n";
+            }
+            i++;
+        }
+        System.out.println(print);
+
+    }
+
+    public static void printFrontiere(CASE[] gridOrigin, int nbcol,List<Integer> frontiere){
+
+        String print="";
+
+        int i=1;
+        CASE[] grid = gridOrigin.clone();
+
+        for(Integer f : frontiere){
+            grid[f] = CASE.BLOW;
+        }
+
+
+        for(CASE c : grid){
+            if(c == CASE.UNDISCOVERED){
+                print+= '□';
+            }else if(c == CASE.EMPTY) {
+                print+='_';
+            }else if(c == CASE.FLAGED){
+                print+= '⚑';
+
+            }else if(c == CASE.BLOW) {
+                print+= '●';
+            }else if(c== CASE.DEFUSED){
+                print+= '◯'; //○
+            }else {
+                print += c.indexValue;
+            }
+            if(i%nbcol ==0 && i!=0){
+                print += "\n";
+            }
+            i++;
+        }
+        System.out.println(print);
+
+    }
+
+        public static void printIndex(CASE[] gridOrigin, int nbcol,Integer toShow){
+
+        String print="";
+
+        int i=1;
+        CASE[] grid = gridOrigin.clone();
+
+            grid[toShow] = CASE.BLOW;
 
 
         for(CASE c : grid){
