1263a14aa5e17edea65f55fd3feee4006752eecc
GitMyCode <marbouchl@gmail.com>
2014-11-08 19:47:38 -0500

    HEAD:  Ajout design, changement order des imgs DESCRIPTION: 	+Les nom des fichiers dans une design ex: design/img5/1.png   	 doivent etre des integers

--- Minesweeper/root/GridView.java
+++ Minesweeper/root/GridView.java
@@ -3,13 +3,16 @@ package root;
 import com.sun.java.util.jar.pack.*;
 import root.ENUM.COUP;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 import java.io.File;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Enumeration;
 
 import static root.ENUM.CASE.*;
@@ -75,19 +78,29 @@ public class GridView extends JPanel {
                 final File folder  = new File(ressource.nextElement().getFile());
                 int i=0;
                 File[] t = folder.listFiles();
-                Arrays.sort(t);
+
+                try {
+
+                    Arrays.sort(t,new compImg());
+                }catch (Exception e){
+                    System.out.println(e);
+                }
                 for (File cellImg : t){
                     java.net.URL imageUrl =  cellImg.toURL();
                     cases[i] =  new ImageIcon(imageUrl).getImage();
+                    BufferedImage im = ImageIO.read(cellImg);
+                    BufferedImage b2 = getScaledInstance(im,caseSize,caseSize,true);
+                    /*RescaleOp rescaleOp = new RescaleOp(0.88f, 20f, null);
+                    rescaleOp.filter(b2, b2);*/
                     BufferedImage bi = new BufferedImage(cases[i].getWidth(null),cases[i].getHeight(null), BufferedImage.TYPE_INT_ARGB);
                     Graphics g = bi.createGraphics();
                     g.drawImage(cases[i],0,0, caseSize,caseSize,null);
-                    cases[i]=  new ImageIcon(bi).getImage();
-
+                    cases[i]=  new ImageIcon(b2).getImage();
                     i++;
                 }
 
             }catch (Exception e){
+                System.out.println(e);
 
             }
     }
@@ -95,10 +108,10 @@ public class GridView extends JPanel {
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
 
-
+        int casePlusSpace = caseSize ;
         for(int i=0; i< (nbcol*nbligne); i++){
-            int x = (i/nbcol)  * caseSize;
-            int y = (i%nbcol)  * caseSize;
+            int x = (i/nbcol)  * casePlusSpace;
+            int y = (i%nbcol)  * casePlusSpace;
 
             g.drawImage(cases[grid.gridPlayerView[i].indexValue],y,x,this);
         }
@@ -140,6 +153,77 @@ public class GridView extends JPanel {
 
             }
         }
+    }
+
+    public class compImg implements Comparator<File>{
+        @Override
+        public int compare (File o1, File o2) {
+            int name1 = Integer.valueOf(o1.getName().split("\\.")[0]);
+            int name2 = Integer.valueOf(o2.getName().split("\\.")[0]);
+            if(name1 < name2){
+            return -1;
+            }
+            if(name1 > name2){
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+
+    /*
+    * https://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html
+    * */
+    public BufferedImage getScaledInstance(BufferedImage img,
+                                           int targetWidth,
+                                           int targetHeight,
+                                           boolean higherQuality)
+    {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ?
+                BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage)img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
     }
 
 
