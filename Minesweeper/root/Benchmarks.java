8c5117ccb7a209b14e41d3913ddfd0ee1a67a231
louib <louib@users.noreply.github.com>
2014-11-19 20:09:00 -0500

    Benchmarks
    Fichier de base pour les benchmarks.

---
+++ Minesweeper/root/Benchmarks.java
@@ -0,0 +1,13 @@
+package root;
+
+public final class Benchmarks {
+
+    private Benchmarks() {
+
+    }
+
+    public static void main(final String[] args) {
+        System.out.println("Yup...");
+    }
+
+}
