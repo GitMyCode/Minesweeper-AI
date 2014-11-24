6aab9ba2a3efea9af8f1693a13257cb37f3839fd
Frédéric Vachon <fredvac@gmail.com>
2014-11-23 20:58:20 -0500

    Style
    - Modif de style
    - Timide réduction de code
    
    Signed-off-by: Frédéric Vachon <fredvac@gmail.com>

--- src/minesweeper/ai/CSPGraph.java
+++ src/minesweeper/ai/CSPGraph.java
@@ -6,8 +6,6 @@ import minesweeper.Coup;
 import minesweeper.ai.utilCSP.Graph;
 import minesweeper.ai.utilCSP.TimeOver;
 
-import static minesweeper.Coup.*;
-
 import static minesweeper.Case.*;
 
 import java.util.*;
@@ -17,21 +15,16 @@ import java.util.*;
  */
 public class CSPGraph implements ArtificialPlayer {
 
-
     /*Timer*/
     private long timer;
     private long remain;
     private boolean END = false;
     private final int LIMITE = 10;
-
-
     private Grid gameGrid;
 
     //Set<Integer> undiscoveredFrontier;
     private List<Integer> nbMatchByFrontier;
     private Integer nbPossibilite = 0;
-
-
     Graph graph;
 
     @Override
@@ -42,7 +35,6 @@ public class CSPGraph implements ArtificialPlayer {
         startTimer(delay);
         Set<Move> movesToPlay;
 
-
         nbPossibilite = 0;
         movesToPlay = grid.checkForSafeMoves();
         if(!movesToPlay.isEmpty()){
@@ -50,8 +42,6 @@ public class CSPGraph implements ArtificialPlayer {
         }
 
         nbMatchByFrontier = new ArrayList<Integer>();
-
-
 
         /*
         * On lance le AI
@@ -63,20 +53,16 @@ public class CSPGraph implements ArtificialPlayer {
             System.out.println("timeout");
         }
 
-
-
-
         /*
         * Une fois qu'on a fini d'analyser on va maintenant choisir les coup a jouer
         *
-        * param: nbMatchByFrontier
-        *           C'est dans cette liste que j'ai gardé le nombre de possibilité pour cette frontiere
-        *           Par exemple un frontiere qui permet 10 combinaison différente de position de flags aura 10 possibilité
-        *       fringeNodes
-        *           C'est les cases non-decouverte qui cotoient une case découverte (un indice)
-        *
-        *
-        * */
+        * params:
+        *   nbMatchByFrontier :
+        *       C'est dans cette liste que j'ai gardé le nombre de possibilité pour cette frontière
+        *       Par exemple, un frontière qui permet 10 combinaisons différentes de positions de flags aura 10 possibilités
+        *   fringeNodes :
+        *       C'est les cases non-découvertes qui côtoient une case découverte (un indice)
+        **/
         if (movesToPlay.isEmpty()) {
 
             System.out.println("essai avec les resultats csp");
@@ -90,7 +76,6 @@ public class CSPGraph implements ArtificialPlayer {
                     if (fn.nbFlagHits == 0) {
                         movesToPlay.add(new Move(fn.indexInGrid, Coup.SHOW));
 
-
                     /*
                     * Si ce noeud a recu un flag sur toute les dispositions valid de flags sur la frontiere
                     * Alors c'est qu'il y a 100% des chance d'avoir un flag
@@ -99,18 +84,12 @@ public class CSPGraph implements ArtificialPlayer {
                         movesToPlay.add(new Move(fn.indexInGrid, Coup.FLAG));
                     }
                 }
-
             }
-
         }
 
-        if (timeUp()) {
-            System.out.println("Time UP!");
-        }
+        if (isTimeUp()) { System.out.println("Time UP!"); }
 
-        /*
-        * Si aucun coup sur a été trouvé alors on essai au hasard
-        * */
+        // Si aucun coup sur a été trouvé alors on essai au hasard
         if (movesToPlay.isEmpty()) {
             List<Integer> legalMoves = new ArrayList<Integer>();
             for (int i = 0; i < grid.length; i++) {
@@ -127,16 +106,7 @@ public class CSPGraph implements ArtificialPlayer {
         return movesToPlay;
     }
 
-
-    @Override
-    public String getName() {
-        return "CSP-Martin";
-    }
-
-
-    /**
-     * C'est le AI en soit. Tout commence par cette method
-     */
+    // C'est le AI en soit, tout commence par cette méthode
     void calculateMoves(Grid g) throws TimeOver {
 
         Case[] grid = g.getCpyPlayerView();
@@ -148,8 +118,7 @@ public class CSPGraph implements ArtificialPlayer {
         * Ces deux listes contiennent toute les frontieres indépendante
         * allHintNode: Les frontieres qui sont des indice
         * allFringeNode: les frontieres qui sont des case non découvertes
-        * */
-
+        **/
         graph = new Graph(g);
         System.out.println("va pour le csp");
         int i = 0;
@@ -158,13 +127,12 @@ public class CSPGraph implements ArtificialPlayer {
             List<Graph.FringeNode> fringeNodes = graph.allFringeNodes.get(i);
             i++;
 
-            //Reset nbPossibilite pour cette frontiere
+            // Reset nbPossibilite pour cette frontiere
             nbPossibilite = 0;
-            recurseCSP(hintBorder, fringeNodes, 0); //   ON LANCE LE CSP SUR CETTE FRONTIERE!!
+            recurseCSP(hintBorder, fringeNodes, 0); // Lancer CSP sur cette frontière
 
             nbMatchByFrontier.add(nbPossibilite);
         }
-
     }
 
     /*
@@ -180,29 +148,19 @@ public class CSPGraph implements ArtificialPlayer {
             de la frontiere incrément leur compter de flag si elle sont flaggé. (Pour calculer les probs)
         3) calcul de toutes les combinaison possible de placement de drapeau autour de la variable présente.
         4) pour chacune des combinaison trouvé. Les drapeau sont placées et on récurse.
-    * */
-
+    **/
     boolean recurseCSP(List<Graph.HintNode> hintNodes, List<Graph.FringeNode> fringeNodes, int index) throws TimeOver {
 
         //Si on dépasse le thinkDelay
-        if (timeUp()) {
+        if (isTimeUp()) {
             throw new TimeOver();
         }
 
-        /*
-        * Vérifie si jusqu`a maintenant toute les variables (la frontiere avec les indices) sont satisfaites
-
-        */
-
-        if (!allFlagsOkay(hintNodes, index)) {
-            return false;
-        }
+        // Vérifie si jusqu`a maintenant toute les variables (la frontiere avec les indices) sont satisfaites
+        if (!allFlagsOkay(hintNodes, index)) { return false; }
 
 
-        /*
-        * Quand on est arrivé au bout de la frontiere et que tout marche!
-        * Une disposition(solution CSP) est trouvé!
-        * */
+        // Quand on est arrivé au bout de la frontiere et que tout marche, une disposition(solution CSP) est trouvé!
         if (index >= hintNodes.size()) {
             /*
             * on passe sur tout les nodes et on check si il y un flag.
@@ -219,23 +177,21 @@ public class CSPGraph implements ArtificialPlayer {
             return true;
         }
 
-
-        //On passe a la prochaine variable a satisfaire
+        // On passe a la prochaine variable a satisfaire
         Graph.HintNode variableToSatisfy = hintNodes.get(index);
         // Update la variable puisqu'on a peut etre flag certainte de ses cases autour.
         variableToSatisfy.updateSurroundingAwareness();
 
-        //Un check pour voir si la variable a trop de flag autour d'elle on backtrack
+        // Un check pour voir si la variable a trop de flag autour d'elle on backtrack
         if (variableToSatisfy.nbFlagToPlace < 0) {
             return false;
         }
-        //Si la variable est déja satisfaite alors on passe a la suivante!
+        // Si la variable est déja satisfaite alors on passe a la suivante!
         if (variableToSatisfy.nbFlagToPlace == 0) {
             return recurseCSP(hintNodes, fringeNodes, index + 1);
         }
 
-
-        //On va chercher cases non decouvertes voisin
+        // On va chercher cases non decouvertes voisin
         Set<Graph.FringeNode> neighborsFringe = variableToSatisfy.connectedFringe;
         List<Graph.FringeNode> undiscoveredFringe = new ArrayList<Graph.FringeNode>();
         for (Graph.FringeNode fn : neighborsFringe) {
@@ -244,23 +200,15 @@ public class CSPGraph implements ArtificialPlayer {
             }
         }
 
-
-
         /*
-        * Okey ca c'est peut etre un peu tricky
         * Mon but est d'avoir tout les combinaisons du nombre de flags qui me reste a placer pour cette variables sur
         * les cases disponibles autour d'elle
         *
         *Ce que vont faire ces 2 ou 3 prochaine ligne c'est : http://fr.wikipedia.org/wiki/Combinaison_(math%C3%A9matiques)
-        *
-        *
         * */
         int[] combination = new int[variableToSatisfy.nbFlagToPlace];
         ArrayList<int[]> listcombination = new ArrayList<int[]>();
-        combinaisonFlag(0, variableToSatisfy.nbFlagToPlace, undiscoveredFringe.size()
-                , combination, listcombination);
-
-
+        combinaisonFlag(0, variableToSatisfy.nbFlagToPlace, undiscoveredFringe.size(), combination, listcombination);
 
         /*
         * Itere sur les combinaisons trouvé
@@ -292,7 +240,6 @@ public class CSPGraph implements ArtificialPlayer {
         return false;
     }
 
-
     boolean allFlagsOkay(List<Graph.HintNode> hintNodes, int nbDone) {
         for (int i = 0; i < nbDone; i++) {
             Graph.HintNode hintNode = hintNodes.get(i);
@@ -314,7 +261,6 @@ public class CSPGraph implements ArtificialPlayer {
         return true;
     }
 
-
     /*
     * C'est simlement l'implementation de C(n,p) exemple : http://calculis.net/combinaison
     * nbFlag  = p
@@ -326,7 +272,7 @@ public class CSPGraph implements ArtificialPlayer {
     *  [0, 2],
     *  [1, 2]
     * */
-    void combinaisonFlag(int index, int nbFlag, int nbCase, int[] combinaison, ArrayList<int[]> listeC) {
+    public void combinaisonFlag(int index, int nbFlag, int nbCase, int[] combinaison, ArrayList<int[]> listeC) {
         if (nbFlag == 0) {
             return;
         }
@@ -350,37 +296,22 @@ public class CSPGraph implements ArtificialPlayer {
         remain = delai;
     }
 
+    private String showTimeRemain() { return ("Time: " + timeRemaining() + " ms"); }
 
-    private String showTimeRemain() {
-        return ("Time: " + (remain - (System.currentTimeMillis() - timer)) + " ms");
+    public long timeRemaining() {
+        long elaspsed = (System.currentTimeMillis() - timer);
+        return remain - elaspsed;
     }
 
-    /**
-     * @return Retourne le temps restant
-     */
-
-
-    long timeRemaining() {
-        long passed = (System.currentTimeMillis() - timer);
-        return remain - passed;
+    public boolean isTimeUp() {
+        END = (timeRemaining() < LIMITE) ? true: END;
+        return END;
     }
 
-    /**
-     * Indique si le temps est écoulé
-     *
-     * @return true si temps écoulé
-     */
-    boolean timeUp() {
-        if (END) {
-            return true;
-        }
-
-        if (timeRemaining() < LIMITE) {
-            END = true;
-            return true;
-        }
-
-        return false;
+    @Override
+    public String getName() {
+        return "CSP-Martin";
     }
+
 
 }
