82777f83871417ed8629b33272db0fb32c35f7bc
louib <louib@users.noreply.github.com>
2014-11-24 21:14:12 -0500

    It's back
    isStepThisDirInGrid est de retour. Après avoir été éradiquée du codebase,
    la fonction sait se montrer irremplaçable, et effectue un retour en
    force, dans toute sa splendeur.

--- src/minesweeper/ai/utilCSP/Graph.java
+++ src/minesweeper/ai/utilCSP/Graph.java
@@ -172,7 +172,7 @@ public class Graph {
             int next = index+gameGrid.step(D);
 
 
-            if (gameGrid.isInGrid(index + gameGrid.step(D)) &&
+            if (gameGrid.isStepThisDirInGrid(D, index) &&
                     !frontiere.contains(next) &&
                     isAFringeNode(grid,next))
             {
