f25757716cca98c6424b982a42db1617c585bc57
Frédéric Vachon <fredvac@gmail.com>
2014-11-25 16:26:22 -0500

    Correction erreur dans Benchmark.java
    Signed-off-by: Frédéric Vachon <fredvac@gmail.com>

--- src/minesweeper/Benchmarks.java
+++ src/minesweeper/Benchmarks.java
@@ -2,13 +2,13 @@ package minesweeper;
 
 import java.util.Set;
 import minesweeper.ai.CSPGraph;
-import minesweeper.ai.RandomAi;
+import minesweeper.ai.RandomArtificialPlayer;
 
 public final class Benchmarks {
 
     public final static int NB_PARTIES = 100;
     public final static int TAILLE_GRILLE = 50;
-    public final static ArtificialPlayer[] JOUEURS = {new RandomAi(), new CSPGraph()};
+    public final static ArtificialPlayer[] JOUEURS = {new RandomArtificialPlayer(), new CSPGraph()};
 
     private Benchmarks() {
 
