0e8aef2b196fd06a6598e977f318de929a33ca5b
GitMyCode <marbouchl@gmail.com>
2014-11-29 16:41:01 -0500

    Enormément de changment
    	* Foward checking
    	* experimentation d'un CSP static (le csp ne devrais pas etre un AI)
    	* Commencement d'un CSP iteratif (plus efficace qu'un recursif)
    	* Ajout de methods pour print dans Util
    	* Changement des les Noeud
    		* HintNode
    			* Pour avoir du foward checking on desactive les fringes qui
    			  invaliderait cette variable. Maintenant getUndiscoveredFringe()
    			  retourne les fringes accessibles (qui ne sont pas desactivé et pas
    			  flaggé)

--- src/minesweeper/ai/utilCSP/Util.java
+++ src/minesweeper/ai/utilCSP/Util.java
@@ -4,6 +4,7 @@ import minesweeper.Case;
 import minesweeper.Coup;
 import minesweeper.Move;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -20,6 +21,48 @@ import java.util.Set;
  *   Nilovna Bascunan-Vasquez
  */
 class Util {
+
+
+
+    public static class BuildPrintGrid {
+
+        char[] charGrid;
+        int cols;
+        public BuildPrintGrid (Case[] grid, int nbcol){
+            cols = nbcol;
+            charGrid = getGridToChar(grid);
+        }
+
+        public void print(){
+            printGrid(charGrid,cols);
+        }
+        public BuildPrintGrid showFLagedFringe (Collection<Graph.Node> nodes) {
+            for (Graph.Node node : nodes) {
+                if (((Graph.FringeNode) node).state == Case.FLAGED) {
+                    charGrid[node.indexInGrid] = '⚐';
+                }
+            }
+            return this;
+        }
+        public BuildPrintGrid showDeactivatedFringe (Collection<Graph.Node> nodes) {
+            for (Graph.Node node : nodes) {
+                if (((Graph.FringeNode) node).isDeactivated) {
+                    charGrid[node.indexInGrid] = 'X';
+                }
+            }
+            return this;
+        }
+        public BuildPrintGrid showThisIndex (int index) {
+            charGrid[index] ='★';
+            return this;
+        }
+
+
+    }
+    public static BuildPrintGrid command(Case[] grid, int nbcol){
+        return new BuildPrintGrid(grid,nbcol);
+    }
+
     public static void printGrid(Case[] grid,int nbcol){
         String print="";
 
@@ -43,6 +86,41 @@ class Util {
             i++;
         }
         System.out.println(print);
+    }
+
+    public static void printGrid(char[] charGrid,int nbcol){
+        String print="";
+        int i=1;
+        for(char c : charGrid){
+            print += c;
+            if(i%nbcol==0 && i!=0){
+                print+= "\n";
+            }
+            i++;
+        }
+        System.out.println(print);
+    }
+    public static char[]getGridToChar(Case[] grid){
+        char[] charGrid = new char[grid.length];
+        int i=0;
+        for(Case c : grid){
+            if(c == Case.UNDISCOVERED){
+
+                charGrid[i]= '□';
+            }else if(c == Case.EMPTY) {
+                charGrid[i] ='_';
+            }else if(c == Case.FLAGED){
+                charGrid[i]= '⚑';
+
+            }else if(c == Case.BLOW) {
+                charGrid[i]= '●';
+            }else {
+                charGrid[i] =(char) ('0'+c.indexValue);
+            }
+            i++;
+        }
+
+        return charGrid;
     }
 
     public static void printAllCoup(Case[] gridOriginal, int nbcol,Set<Move> coups){
@@ -100,6 +178,27 @@ class Util {
         }
     }
 
+    public static void printDeactivatedFringe(Case[] gridOrigin, int nbcol,Collection<Graph.Node> nodes){
+
+        char[] gridChar = getGridToChar(gridOrigin);
+        for(Graph.Node node : nodes){
+            if(((Graph.FringeNode)node).isDeactivated){
+                gridChar[node.indexInGrid] = 'X';
+            }
+        }
+        printGrid(gridChar,nbcol);
+    }
+    public static void printFlaggedFringe(Case[] gridOrigin, int nbcol,Collection<Graph.Node> nodes){
+
+        char[] gridChar = getGridToChar(gridOrigin);
+        for(Graph.Node node : nodes){
+            if(((Graph.FringeNode)node).state == Case.FLAGED){
+                gridChar[node.indexInGrid] = '⚐';
+            }
+        }
+        printGrid(gridChar,nbcol);
+    }
+
     public static void printFrontiere(Case[] gridOrigin, int nbcol,Set<Integer> frontiere){
 
         String print="";
@@ -136,7 +235,7 @@ class Util {
 
     }
 
-    public static void printFrontiereNode(Case[] gridOrigin, int nbcol,List<? extends Graph.Node> frontiere){
+    public static void printFrontiereNode(Case[] gridOrigin, int nbcol,Collection<? extends Graph.Node> frontiere){
 
         String print="";
 
