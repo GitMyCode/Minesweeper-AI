b09f7f8595770f35f3732e47c231edabb09c7b44
GitMyCode <marbouchl@gmail.com>
2014-11-30 18:45:44 -0500

    Ajout de BenchmarksCSP
    C'est une classe avec un main qui marche de la même maniere que Benchmarks
    Il va executer une liste de CSP et afficher les resultat. Mais il test avec des
    grilles préfaites.
    
    Par exemple voici le resultat pour SimpleCSP et ensuite FowardChecking sur le
    meme fichier
    
    Simple CSP                              Foward checking
    --------- big-fringe1 ---------         --------- big-fringe1 ---------
    frontiere (0) :0 ms                     frontiere (0) :0 ms
    frontiere (1) :0 ms                     frontiere (1) :0 ms
    frontiere (2) :0 ms                     frontiere (2) :0 ms
    frontiere (3) :1333 ms                  frontiere (3) :930 ms
    frontiere (4) :0 ms                     frontiere (4) :0 ms
    frontiere (5) :0 ms                     frontiere (5) :0 ms
    
    NB: dépendamment l'ordre d'execution des strategies la JVM va optimiser les
    opérations les plus fréquentes. Donc le premier test donne toujours un résultat
    plus lent.

---
+++ src/minesweeper/BenchmarksCSP.java
@@ -0,0 +1,67 @@
+package minesweeper;
+
+import minesweeper.ai.dataRepresentation.Graph;
+import minesweeper.ai.strategyCSP.FowardCheckCSP;
+import minesweeper.ai.strategyCSP.SimpleCSP;
+import minesweeper.ai.strategyCSP.StrategyCSP;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by MB on 11/30/2014.
+ */
+public class BenchmarksCSP {
+
+    public final static StrategyCSP[] CSPS = {
+            new SimpleCSP(),
+            new FowardCheckCSP()
+
+    };
+
+
+    public static void main(String[] args) {
+
+        File directory = new File("src\\minesweeper\\ai\\utilCSP\\benchmarkGridCSP");
+
+        File[] allTestFile = directory.listFiles();
+        List<String[]> logsList = new ArrayList<String[]>();
+        for(StrategyCSP csp : CSPS){
+            System.out.println(csp.strategyToString());
+            csp.addLineToExecutionLog(csp.strategyToString());
+            for (File testFile : allTestFile) {
+                Grid testGrid = new Grid(testFile);
+                Graph testGraph = new Graph(testGrid);
+                csp.addLineToExecutionLog("--------- " + testFile.getName() + " ---------");
+                csp.executeCSPonGraph(testGraph);
+
+            }
+            logsList.add(csp.getExecutionLog().split("\n"));
+        }
+
+        for (int logLine = 0; logLine < logsList.get(0).length; logLine++) {
+            String logStringLine = "";
+            for(int cspLog = 0 ; cspLog < logsList.size(); cspLog++ ){
+                String logLineForThisCSP = logsList.get(cspLog)[logLine];
+                String end = "";
+                if(cspLog == logsList.size()-1){
+                    end = "\n";
+                }else {
+                    int spaceSize = 40 - logLineForThisCSP.length();
+                    char[] spaceArray = new char[spaceSize];
+                    Arrays.fill(spaceArray, ' ');
+                    end = new String(spaceArray);
+                }
+
+
+                logStringLine += logLineForThisCSP+end;
+            }
+            System.out.print(logStringLine);
+        }
+
+    }
+
+
+}
