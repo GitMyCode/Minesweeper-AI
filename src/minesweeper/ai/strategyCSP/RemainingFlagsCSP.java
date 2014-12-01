d6e0b429512238bf2cd0af494ae2fb809688c36f
Frédéric Vachon <fredvac@gmail.com>
2014-11-30 23:19:20 -0500

    Implémentation de strategyCSP
    Signed-off-by: Frédéric Vachon <fredvac@gmail.com>

--- src/minesweeper/ai/strategyCSP/RemainingFlagsCSP.java
+++ src/minesweeper/ai/strategyCSP/RemainingFlagsCSP.java
@@ -74,4 +74,9 @@ public class RemainingFlagsCSP extends SimpleCSP {
 
         return count;
     }
+
+    @Override
+    public String strategyToString() {
+        return "Remaining Flags CSP";
+    }
 }
